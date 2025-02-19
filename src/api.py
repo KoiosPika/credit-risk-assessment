57f25061ed69a1b7f724fe0a68808a2862d6abcb
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2025-02-18 21:24:55 -0800

    created dockerfile and uploaded model to S3

--- src/api.py
+++ src/api.py
@@ -1,20 +1,36 @@
 from flask import Flask, request, jsonify
 import joblib
-import pandas as pd
 from flask_cors import CORS
+import pandas as pd
+import os
+import boto3
 
 app = Flask(__name__)
 CORS(app)
 
-# Load the trained model
-model = joblib.load("model_pipeline.pkl")
+# AWS S3 configure
+S3_BUCKET = "credit-risk-assessment"
+MODEL_FILE = "model_pipeline.pkl"
+LOCAL_MODEL_PATH = "/tmp/model_pipeline.pkl"
+
+# Download model from S3
+s3 = boto3.client("s3")
+
+def download_model():
+    if not os.path.exists(LOCAL_MODEL_PATH):
+        print("Downloading model from s3")
+        s3.download_file(S3_BUCKET,MODEL_FILE,LOCAL_MODEL_PATH)
+        print("Download complete.")
+    return joblib.load(LOCAL_MODEL_PATH)
+
+model = download_model()
 
 @app.route('/predict', methods=['POST'])
 def predict():
     """API endpoint to predict credit risk from JSON input."""
     try:
         data = request.get_json()
-        df = pd.DataFrame([data])  # Convert JSON to DataFrame
+        df = pd.DataFrame([data])
         prediction = model.predict(df)
         return jsonify({'credit_risk': int(prediction[0])})
     except Exception as e:
